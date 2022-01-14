a10684c025d9bcf7643355e193d9d9a87c3be21b
Alaa-Alkabariti <alaakabariti1996@gmail.com>
2022-01-14 12:33:43 +0200

    Friday commit

--- Control/Control.js
+++ Control/Control.js
@@ -1,198 +1,209 @@
-/* let formData = document.getElementById('inputs');
+//Profile
 
-if (formData) {
-    formData.addEventListener('submit' , handling);
-}
-
-
-function handling(e) {
-    e.preventDefault();
-
-    let inputOne = document.getElementById('name').value;
-    let inputTwo = document.getElementById('desc').value;
-
-    fetch('https://61bf323ab25c3a00173f4cff.mockapi.io/profile/4' , {
-        method : 'PUT',
-        body : JSON.stringify({
-            name : inputOne,
-            discription : inputTwo,
-        }),
-        headers : {
-            'Content-Type' : 'application/json'
-        }
-    }).then(response => response.json())
-} */
-
+//Update profile details starts here
 let profileData = document.getElementById("profileForm");
 //to check if the page has a form html code
 if (profileData) {
   profileData.addEventListener("submit", handleEventListener);
-
-  /*    let addNew = document.getElementById("two")
-    addNew.addEventListener("click", btnClick, false) */
 }
 
 function handleEventListener(e) {
   e.preventDefault();
   let nameProperty = document.getElementById("userName").value;
-  let idProperty = document.getElementById("userId").value;
   let titleProperty = document.getElementById("userTitle").value;
   let descProperty = document.getElementById("userdesc").value;
   let imageProperty = document.getElementById("userImage").value;
 
-  const userProfileDetails = [
-    nameProperty,
-    idProperty,
-    titleProperty,
-    descProperty,
-    imageProperty,
-  ];
-
-  /*  const profileValues = [nameProperty , titleProperty , descProperty , imageProperty]; */
-
-  /* console.log(imageProperty); */
-  fetch(`https://61bf323ab25c3a00173f4cff.mockapi.io/profile/1`, {
+  fetch(`https://61bf323ab25c3a00173f4cff.mockapi.io/profile/2`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      name: userProfileDetails[0],
-      id: userProfileDetails[1],
-      title: userProfileDetails[2],
-      discription: userProfileDetails[3],
-      img: userProfileDetails[4],
+      name: nameProperty,
+      title: titleProperty,
+      discription: descProperty,
+      img: imageProperty,
     }),
-  })
-    .then((response) => response.json())
-  //without another then or even catch, we dont need to handle with the return data m just display them
+  }).then((response) => response.json());
 }
+//Update profile details ends here
 
-//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//Skills
 
 //Skills section starts here
-//get the form element and assign it into the
+//get the form element and assign it into the   >>>> Done
+function handleSkillsForm() {
+  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills", {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let userProfileSkills = data.map((skill) => {
+        return `
+        <div class="skill">
+        <div class="s1">
+            <p id="skill" class="skillItem"> Skill Name:  ${skill.skill}</p>
+        </div>
+        <div class="s2">
+            <p id="expe" class="skillItem"> Experience: ${skill.Experience}</p>
+        </div>  
+        <button id="Update" class="updateSkillBtn" onclick="updateSkill(${skill.id})">Update</button>
+        <button id="Delete" class="deleteSkillBtn" onclick="deleteSkill(${skill.id})">Delete</button>
+    </div>`;
+      });
+
+      document.querySelector(".viewSkills").innerHTML = userProfileSkills;
+      //Why I had an error when I I used getElementByClassName
+      Object.values(document.getElementsByClassName("viewSkills")).map((el) => {
+        el.style.width = "100%";
+        el.style.minWidth = "200px";
+        el.style.border = "2px solid #eeeeee";
+        el.style.margin = "10px"; /* 
+          el.style.padding = "10px" */
+        el.style.display = "flex";
+        el.style.flexDirection = "row";
+        el.style.justifyContent = "center";
+        el.style.alignItems = "start";
+        el.style.boxShadow = "3px 3px 3px 0px rgba(227,222,222,0.75)";
+        el.style.boxShadow = "3px 3px 3px 0px rgba(227,222,222,0.75)";
+        el.style.backgroundColor = "transparent";
+      }); /* .join("") */
+    });
+}
+handleSkillsForm();
+//Get all the skills and display them ends here
+
+//Create new skill and display it in the skills list
 let skillsForm = document.getElementById("skillsForm");
 if (skillsForm) {
-  skillsForm.addEventListener("submit", handleSkillsForm);
+  skillsForm.addEventListener("submit", addSkill);
 }
 
-function handleSkillsForm(e) {
-  e.preventDefault(); //to prevent the browser auto refresh
-  let skillName = document.getElementById("skill").value;
-  let skillExperience = document.getElementById("Experience").value;
-  let skillId = document.getElementById("id").value;
+function addSkill(e) {
+  e.preventDefault();
 
-  const skillItem = [skillName, skillExperience, skillId];
+  let newSkill = document.getElementById("skill").value;
+  let newExp = document.getElementById("expe").value;
 
-  console.log(skillName);
-  console.log(skillExperience);
-  console.log(skillId);
-
-  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills/55", {
-    method: "PUT",
+  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills", {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      skill: skillItem[0],
-      experience: skillItem[1],
-      id: skillItem[2],
+      skill: newSkill,
+      Experience: newExp,
     }),
   })
     .then((response) => response.json())
-
-  function addSkill(/* e */) {
-    /* e.preventDefault(); */
-
-    fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        skill: "jquery",
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        //for testing
-        let newSkill = document.createElement("p");
-        skillsForm.appendChild(newSkill);
-        newSkill.innerHTML = data.skill;
-      });
-  }
-
-  function deleteSkill() {
-    fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills/54 ", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
-  deleteSkill();
-
-  //Skills section ends here
-
-  /*more than one button in form:   https://eloquentjavascript.net/15_event.html*/
-
-  ///////////////////////////////////////////////////////////////////////////
-
+    .then((skill) => handleSkillsForm());
 }
 
-
-
-let gallaeryForm = document.getElementById("galForm");
-if (gallaeryForm) {
-  gallaeryForm.addEventListener("submit", addImageToGallery);
+function deleteSkill(id) {  
+  fetch(`https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills/${id} `, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((skill) => handleSkillsForm());
 }
 
-function addImageToGallery(e) {
-  e.preventDefault();
+function updateSkill(id) {
+  let newSkill = document.getElementById("skill").value;
+  let newExp = document.getElementById("expe").value;
 
-  let imageLink = document.getElementById("imageLink").value;
-  let imageDescription = document.getElementById("description").value;
-  let imageId = document.getElementById("id").value;
-
-
-
-  const arrayOfImage = [imageLink, imageDescription, imageId];
-
-  console.log(arrayOfImage)
-
-  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/gallery/1  ", {
+  fetch(`https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills/${id} `, {
     method: "PUT",
-    
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      img: arrayOfImage[0],
-      description: arrayOfImage[1],
-      id: arrayOfImage[2],
+      skill: newSkill,
+      Experience: newExp,
     }),
   })
-    .then(response => { return response.json()})
+    .then((response) => response.json())
+    .then((skill) => handleSkillsForm());
 }
 
+//Skills section ends here
+
+/*more than one button in form:   https://eloquentjavascript.net/15_event.html*/
+/* } */
 
 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+//Gallery
+
+//Update gallery details starts here
+
+let galleryForm = document.getElementById("galleryForm");
+if (galleryForm) {
+  galleryForm.addEventListener("submit", updateImageOfGallery);
+}
+
+function updateImageOfGallery(e) {
+  e.preventDefault();
+
+  let imageLink = document.getElementById("galleryImg").value;
+  let imageDescription = document.getElementById("galleryDescription").value;
+
+  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/gallery/28", {
+    method: "PUT",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      img: imageLink,
+      description: imageDescription,
+    }),
+  }).then((response) => {
+    return response.json();
+  });
+}
+//Update gallery details ends here
 
 function AddNewImg() {
-    fetch('https://61bf323ab25c3a00173f4cff.mockapi.io/gallery' , {
-        method : 'POST',
-        headers : {
-            'Content-Type' : 'application/json'
-        },
-        body : JSON.stringify(
-            {
-                img: "https://www.google.com/search?q=images&sxsrf=AOaemvLUNBF1wQNsQwhekTM36LI0c5A0UQ:1641936967220&tbm=isch&source=iu&ictx=1&vet=1&fir=v1A5vQPzRgfCGM%252C7T8fKkixbRnWvM%252C_%253BL8xfQakH9a8tJM%252CBa_eiczVaD9-zM%252C_%253B2eIb5BjhQiiZzM%252CnBiD9BWYMB87aM%252C_%253BLK6S_eMkLDVwQM%252CB51x0PBR9KNzvM%252C_%253BarFfSjMu_GX7sM%252CUkcvm3PybD5jEM%252C_%253BITUG1uFAV1aQ9M%252CX9GBbDAacv-dWM%252C_%253BOFjLbX408Yt9iM%252CjMPx2dPKbYbCVM%252C_%253BtTplitM2kjOQtM%252C-UStXW0dQEx4SM%252C_%253BeXUC-3WyVcZa-M%252CfcNz05h1Ca2eyM%252C_%253B2DNOEjVi-CBaYM%252CAOz9-XMe1ixZJM%252C_%253B8EekYKjMUCitwM%252CUkcvm3PybD5jEM%252C_%253BsSAWficq0VlQLM%252CG9GbNX6HcZ2O_M%252C_%253Bwp1tdfttzeGYZM%252CZaycYywhXLmIVM%252C_&usg=AI4_-kRNhU4YWdoQJrqkzp7F_5496CF8Gw&sa=X&ved=2ahUKEwigr7Lt06r1AhURSvEDHSCXDJAQ9QF6BAgKEAE#imgrc=L8xfQakH9a8tJM",
-                description:"Description text",
-                id: '2'
-            }
-        )
-    }).then(response => response.json())
-    .then(data => console.log(data))
+  let imageLink = document.getElementById("galleryImg").value;
+  let imageDescription = document.getElementById("galleryDescription").value;
+
+  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/gallery", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      img: imageLink,
+      description: imageDescription,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
+/* AddNewImg(); */
+
+
+
+
+/*File uploading starts here*/
+/*File uploading ends here*/
--- Display/Display.js
+++ Display/Display.js
@@ -1,63 +1,70 @@
-/* function getUserName() {
-    fetch('https://61bf323ab25c3a00173f4cff.mockapi.io/profile' , {
-        method : 'GET'
-    }).then(response => {return response.json()})
-    .then((data) => {
-        userName.innerHTML = data[0].name,
-        discription.innerHTML = data[0].discription
-    })
-
-}
-
-getUserName(); */
-
+//Update user profile details starts  here
 const userNameD = document.getElementById("userName");
 const userTitleD = document.getElementById("userTitle");
 const userDescD = document.getElementById("userDescription");
 const userImageD = document.getElementById("userImage");
 
 function getUserData() {
-  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profile", {
+  // fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profile", {
+  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profile/2", {
     method: "GET",
   })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      userNameD.innerHTML = data[0].name;
-      userTitleD.innerHTML = data[0].title;
-      userImageD.src = data[0].img;
-      userDescD.innerHTML = data[0].discription;
-
-      console.log(userImageD.src);
+      userNameD.innerHTML = data.name;
+      userTitleD.innerHTML = data.title;
+      userImageD.src = data.img;
+      userDescD.innerHTML = data.discription;
     });
 }
 
 getUserData();
+//Update user profile details ends here
 
 ///////////////////////////////////////////////////////////////////////////
 
 //Skills sections strts here
 
-const skill1 = document.getElementById("skill1");
-
+// const skill1 = document.getElementById("skill1");
 const skillId = document.getElementById("skillId");
 const skillExperience = document.getElementById("skillExperience");
 const skillName = document.getElementById("skillName");
 
-const alluserSkillDetails = [skillId, skillExperience, skillName];
 
 function getSkills() {
   fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills", {
     method: "GET",
   })
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => {return response.json();})
     .then((data) => {
-      (skillId.innerHTML = data[1].skill),
-        (skillExperience.innerHTML = data[0].experience);
-      skillName.innerHTML = data[0].id;
+      let userProfileSkills = data.map((skill) => {
+        return `
+        <div style="min-width:200px; margin:10px; width:50%; background-color:#eeeeee; color:#ffffff border="10px solid #ff0000; box-shadow:3px 3px 3px #ff0000" class="skillItem">
+                <p id="skillName">Skill Name: ${skill.skill}</p>
+                <p id="skillExperience">Experience:${skill.Experience}</p>
+            </div>`;
+      });
+
+      document.querySelector(".skillItem").innerHTML = userProfileSkills;
+      //Why I had an error when I I used getElementByClassName
+      Object.values(document.getElementsByClassName("skillsSection")).map((el) => {
+        el.style.width = "100%";
+        el.style.height = "auto";
+        el.style.minWidth = "200px";
+        el.style.border = "2px solid #eeeeee";
+        el.style.margin = "10px"; /* 
+          el.style.padding = "10px" */
+        el.style.display = "flex";
+        el.style.flexDirection = "row";
+        el.style.justifyContent = "center";
+        el.style.alignItems = "start";
+        el.style.flexWrap = "wrap";
+        el.style.boxShadow = "3px 3px 3px 0px rgba(227,222,222,0.75)";
+        el.style.boxShadow = "3px 3px 3px 0px rgba(227,222,222,0.75)";
+        /* el.style.backgroundColor = "transparent"; */
+      });   
     });
 }
 
@@ -66,22 +73,31 @@ getSkills();
 //Skills sections ends here
 
 //Display the new added skill starts here
-const newSkill = document.getElementById("newSkill");
 
-function displayNewSkill() {
-  fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      newSkill.innerHTML = data[0].skill;
-    });
-}
+// const newSkill = document.getElementById("newSkill");
 
-displayNewSkill();
+// function displayNewSkill() {
+//   fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/profileSkills", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+
+//       `<div>
+//       <p id="skillName"></p> <button onclick="deleteSkill()" id="del">Delete</button>
+//       <p id="skillExperience"></p> <button onclick="deleteSkill()" id="del">Delete</button>
+//       <p id="skillId"></p> <button onclick="deleteSkill()" id="del">Delete</button>
+//       </div>`
+
+//       newSkill.innerHTML = data[0].skill;
+//     });
+// }
+
+// displayNewSkill();
+
 //Display the new added skill ends here
 
 //delete skill starts here
@@ -96,11 +112,7 @@ function deleteSkill(id) {
   });
 }
 
-/* fetch the gallery images starts here*/
-const galleryImg1 = document.getElementById("gal1");
-const galleryImg2 = document.getElementById("gal2");
-const galleryImg3 = document.getElementById("gal3");
-function getGalleryImage() {
+function getAllGalleryImages() {
   fetch("https://61bf323ab25c3a00173f4cff.mockapi.io/gallery", {
     method: "GET",
   })
@@ -108,13 +120,43 @@ function getGalleryImage() {
       return response.json();
     })
     .then((data) => {
-      galleryImg1.src = data[0].img;
-      galleryImg2.src = data[0].img;
-      galleryImg3.src = data[0].img;
+      let userProfileDisplaying = data.map((user) => {
+        return `
+        <div class="imageHere" style="margin-top:100px"> 
+        <button style="height:20px; width:30px; text-align:center; margin-left:-20px"  onclick="deleteOneUserFunc(${user.id})" class="btn"><i style="color:white" class="fas fa-trash-alt"></i></button>    
+        <img style="width:100%; height:200px;" src="${user.img}"/>
+        </div>`;
+      });
 
-      console.log(userImageD.src);
+      document.querySelector(".gallerySection").innerHTML =
+        userProfileDisplaying;
+      //Why I had an error when I I used getElementByClassName
+      Object.values(document.getElementsByClassName("imageHere")).map((el) => {
+        el.style.width = "20%";
+        el.style.minWidth = "200px";
+        el.style.border = ".5px solid #eeeeee";
+        el.style.margin = "10px"; /* 
+          el.style.padding = "10px" */
+        el.style.display = "flex";
+        el.style.flexDirection = "row";
+        el.style.justifyContent = "end";
+        el.style.alignItems = "start";
+        el.style.boxShadow = "3px 3px 3px 0px rgba(227,222,222,0.75)";
+        el.style.boxShadow = "3px 3px 3px 0px rgba(227,222,222,0.75)";
+        el.style.backgroundColor = "transparent";
+      }); /* .join("") */
     });
 }
 
-getGalleryImage();
-/*fethch the gallery images ends here*/
+getAllGalleryImages();
+
+function deleteOneUserFunc(id) {
+  fetch(`https://61bf323ab25c3a00173f4cff.mockapi.io/gallery/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => getAllGalleryImages());
+}
